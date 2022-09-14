84fad49a8325ee4fec7dd66f4169b43386786c5c
Kaua <kauajuno@gmail.com>
2022-09-14 09:19:21 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+import os
+
+parent_dir = "C:/Users/kauaj/Desktop/Estudos/IPfromScratch/exercícios/"
+
+# Path
+for i in range(164):
+    directory = f"ex{i+1}"
+    path = os.path.join(parent_dir, directory)
+
+    os.mkdir(path)
+    print("Directory '% s' created" % directory)
+"""
+
+for i in range(164):
+    with open(f"ex{i+1}/ex{i+1}.c", mode='w') as f:
+        f.write("#include <stdio.h>\n\nint main(){}")
